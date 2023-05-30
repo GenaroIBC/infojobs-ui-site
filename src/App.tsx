0f6cc46368eb63641c8a6b7a18932e426f7ca029
genaroibc <bonavitaismael@gmail.com>
2023-05-30 09:47:45 -0300

    [FEAT]: add navigation aside

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,40 @@
-export default function App() {
-  return <div>App</div>
+const ASIDE_LINKS = [
+  {
+    href: "#accordion",
+    title: "Accordion"
+  },
+  {
+    href: "#breadcrumb",
+    title: "Breadcrumb"
+  },
+  {
+    href: "#tooltip",
+    title: "Tooltip"
+  },
+  {
+    href: "#button",
+    title: "Button"
+  }
+]
+
+function App() {
+  return (
+    <>
+      <section className="grid grid-cols-4 relative max-w-page-max-width mx-auto">
+        <aside className="col-span-1 p-4 sticky h-screen top-0">
+          <nav>
+            <ul className="flex flex-col gap-2 mt-12">
+              {ASIDE_LINKS.map(({ href, title }) => (
+                <li key={href}>
+                  <a href={href}>{title}</a>
+                </li>
+              ))}
+            </ul>
+          </nav>
+        </aside>
+      </section>
+    </>
+  )
 }
+
+export default App
