c99f62b05059fd31f221ccaea412a1108a15c18d
genaroibc <bonavitaismael@gmail.com>
2023-05-30 09:48:00 -0300

    [FEAT]: add footer

--- src/App.tsx
+++ src/App.tsx
@@ -33,6 +33,20 @@ function App() {
           </nav>
         </aside>
       </section>
+
+      <div className="bg-slate-950">
+        <footer className="max-w-page-max-width mx-auto text-gray-50 flex flex-col gap-4 w-full mt-12 py-12 px-4">
+          <p className="text-center">
+            Developed with ❤️ by <a href="https://gena.dev">Genaro Bonavita</a>{" "}
+            in 2023
+          </p>
+          <p className="text-center">
+            <a href="https://github.com/genaroibc/infojobs-ui">
+              Visit InfoJobs UI on GitHub
+            </a>
+          </p>
+        </footer>
+      </div>
     </>
   )
 }
